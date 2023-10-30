9839484674e4d4864388f3fc13388733f0814333
sumitMac <sumit.kumar@Technoidentity.com>
2023-10-30 16:26:20 +0530

    remove react query imports and comments parts

--- shoping-cart/src/main.jsx
+++ shoping-cart/src/main.jsx
@@ -2,20 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Home, dataLoader } from "./components/Home";
 import { About } from "./components/About";
 import { Cart } from "./components/Cart";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-      cacheTime: Infinity,
-    },
-  },
-});
 
 const router = createBrowserRouter([
   {
@@ -31,8 +21,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
