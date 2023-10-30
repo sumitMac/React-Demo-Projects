4b30db8ec757c92aaaeb4ff360e87a45daab66ea
sumitMac <sumit.kumar@Technoidentity.com>
2023-10-30 11:02:58 +0530

    Router updated

--- shoping-cart/src/App.jsx
+++ shoping-cart/src/App.jsx
@@ -1,23 +1,12 @@
 import { Header } from "./components/Header";
+import { Outlet } from "react-router-dom";
 
-import { Route, Routes } from "react-router-dom";
-import { About } from "./components/About";
-import { Home } from "./components/Home";
-import { Cart } from "./components/Cart";
-
-function App() {
+ export const App=()=> {
   return (
     <>
       <Header />
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </div>
+      <Outlet/>
     </>
   );
 }
 
-export default App;
--- shoping-cart/src/main.jsx
+++ shoping-cart/src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import {App} from './App'
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {Home} from './components/Home';
+import {About} from './components/About';
+import { Cart } from './components/Cart';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,12 +17,22 @@ const queryClient = new QueryClient({
   },
 });
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/cart", element: <Cart /> },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <App />
+      <RouterProvider router={router} />
       </QueryClientProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
